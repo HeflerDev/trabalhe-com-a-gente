5803df7e8a9e3fbbdc6b53df14661f32bba4f928
Hefler <heflerdev@gmail.com>
2025-09-24 20:26:50 -0300

    Load latest changes on main file

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,31 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchBar } from './components/search-bar/search-bar';
+import { Cat, CatImage } from './services/cat';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, SearchBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('client');
+
+  constructor(private catService: Cat) { }
+
+  public images: CatImage[] = []
+
+  onSearch(term: string) {
+    if (!term) {
+      this.images = [];
+      return;
+    }
+
+    this.catService.getImages(10, 0, 'RAND', 1, undefined, undefined, term)
+      .subscribe((images) => {
+        this.images = images;
+      });
+  }
 }
