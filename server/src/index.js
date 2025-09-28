b46553b1efdee0aa3796373e5b8a6a3136e6e66c
Hefler <heflerdev@gmail.com>
2025-09-28 19:43:22 -0300

    Forgot to register html

--- server/src/index.js
+++ server/src/index.js
@@ -18,6 +18,10 @@ app.use(bodyParser.json());
 
 if (process.env.NODE_ENV !== "production") {
   app.use(express.static(path.join(__dirname, "./public/client/browser")));
+
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/client/browser/index.html"));
+  });
 }
 
 const server = new ApolloServer({
